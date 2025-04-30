403ead7747ee3d01bc3f5e028e30a8fd0e16e496
xDepcio <olek.drwal@gmail.com>
2025-04-30 19:54:09 +0200

    moved outputs to outpust.tf file

--- tf/main.tf
+++ tf/main.tf
@@ -132,38 +132,3 @@ resource "helm_release" "nginx_ingress" {
     azurerm_kubernetes_cluster.aks
   ]
 }
-
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "acr_login_server" {
-  description = "The FQDN of the Azure Container Registry."
-  value       = azurerm_container_registry.acr.login_server
-}
-
-output "acr_id" {
-  description = "The ID of the Azure Container Registry."
-  value       = azurerm_container_registry.acr.id
-}
-
-output "aks_cluster_name" {
-  description = "The name of the AKS cluster."
-  value       = azurerm_kubernetes_cluster.aks.name
-}
-
-output "aks_cluster_id" {
-  description = "The ID of the AKS cluster."
-  value       = azurerm_kubernetes_cluster.aks.id
-}
-
-output "aks_kube_config_raw" {
-  description = "Raw Kubernetes configuration for the AKS cluster. Use with caution."
-  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive   = true
-}
-
-output "aks_get_credentials_command" {
-  description = "Command to run using Azure CLI to configure kubectl."
-  value       = "az aks get-credentials --resource-group ${azurerm_resource_group.rg.name} --name ${azurerm_kubernetes_cluster.aks.name}"
-}
